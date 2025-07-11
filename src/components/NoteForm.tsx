d2559f36bb936d17712ad0346e401aaf54f97987
M Saepul Hasbi <xhsprojects@gmail.com>
2025-07-11 08:20:13 +0000

    malah jadi gitu

--- src/components/NoteForm.tsx
+++ src/components/NoteForm.tsx
@@ -96,7 +96,7 @@ export function NoteForm({ isOpen, onOpenChange, noteToEdit }: NoteFormProps) {
         </DialogHeader>
         <Form {...form}>
           <form onSubmit={form.handleSubmit(handleSubmit)} className="flex flex-1 flex-col overflow-hidden">
-            <div className="flex-1 overflow-y-auto px-6 space-y-4">
+            <div className="flex-1 space-y-4 overflow-y-auto px-6">
                 <FormField
                 control={form.control}
                 name="title"
